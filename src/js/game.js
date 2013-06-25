4a3394735a97888ddff72fbe66c93f123714ee6a
alvare <welcometothechango@gmail.com>
2013-06-25 17:05:50 -0300

    states

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,27 @@
+player = gamvas.Actor.extend({
+    create: function(name, x, y, controls){
+        this._super(name, x, y);
+        var st = gamvas.state.getCurrentState();
+        this.setFile(st.resource.getImage('rsc/'+ this.name + '.png'));
+        var defaultState = this.getCurrentState();
+
+        var speed = 100;
+        
+        defaultState.update = function(t){
+            if (gamvas.key.isPressed(contols))
+                console.log(p);
+            if (gamvas.key.isPressed(gamvas.key.LEFT))
+                this.actor.move(speed*t, 0);
+        }
+    }
+});
+
 helloState = gamvas.State.extend({
-        draw: function(t) {
+        init: function(){
+            this.addActor(new player('p1', 10, 10, {'up': gamvas.key.UP, 'left': gamvas.key.LEFT}));
+            this.addActor(new player('p2', -20, 10, 3));
+        },
+        draw: function(t){
             this.c.fillStyle = '#fff';
             this.c.font = 'bold 20px sans-serif';
             this.c.textAlign = 'center';
@@ -7,7 +29,7 @@ helloState = gamvas.State.extend({
         }
 });
 
-gamvas.event.addOnLoad(function() {
+gamvas.event.addOnLoad(function(){
     gamvas.state.addState(new helloState('helloworld'));
     gamvas.start('gameCanvas');
 });
