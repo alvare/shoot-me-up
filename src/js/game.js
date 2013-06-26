7e3a57f837a2b1a45f3ea52b568752edb0c37530
alvare <welcometothechango@gmail.com>
2013-06-26 12:53:04 -0300

    rotation 2

--- src/js/game.js
+++ src/js/game.js
@@ -1,32 +1,33 @@
 player = gamvas.Actor.extend({
-    create: function(name, x, y, controls){
+    create: function(name, x, y, controls, angle){
         this._super(name, x, y);
         var st = gamvas.state.getCurrentState();
         this.setFile(st.resource.getImage('rsc/'+ this.name + '.png'));
         var defaultState = this.getCurrentState();
-        this.setCenter(25, 25);
+        this.setCenter(20, 25);
+        this.setRotation(-angle);
 
         var speed = 0;
-            accel = 0,
-            angle = 0,
-            friction = -1,
+            angle = angle,
+            pump = 90,
+            friction = 0.5,
             angspeed = 0.05;
         
         defaultState.update = function(t){
-            accel = 0;
+            var accel = 0;
             if (gamvas.key.isPressed(controls.up)){
-                accel = 5;
-                console.log(this.actor.center, this.actor.position);
+                accel = pump;
+            } else if (gamvas.key.isPressed(controls.down)){
+                accel = -pump;
             }
             if (gamvas.key.isPressed(controls.left)){
                 angle = (angle + angspeed) % (2 * Math.PI);
                 this.actor.setRotation(-angle);
-            }
-            if (gamvas.key.isPressed(controls.right)){
+            } else if (gamvas.key.isPressed(controls.right)){
                 angle = (angle - angspeed) % (2 * Math.PI);
                 this.actor.setRotation(-angle);
             }
-            speed = speed + accel
+            speed = speed*friction + accel
             this.actor.move(Math.cos(angle) * speed * t, -1 * Math.sin(angle) * speed * t);
         }
     }
@@ -34,7 +35,8 @@ player = gamvas.Actor.extend({
 
 startState = gamvas.State.extend({
         init: function(){
-            this.addActor(new player('p1', 10, 10, {'up': gamvas.key.UP, 'left': gamvas.key.LEFT, 'right': gamvas.key.RIGHT}));
+            this.addActor(new player('p1', 200, 0, {'up': gamvas.key.UP, 'left': gamvas.key.LEFT, 'right': gamvas.key.RIGHT, 'down': gamvas.key.DOWN}, Math.PI));
+            this.addActor(new player('p2', -200, 0, {'up': gamvas.key.W, 'left': gamvas.key.A, 'right': gamvas.key.D, 'down': gamvas.key.S}, 0));
         },
         draw: function(t){
         }
