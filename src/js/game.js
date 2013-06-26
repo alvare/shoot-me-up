b7fe799ef18d5e91ef05ba47580b67220d974075
alvare <welcometothechango@gmail.com>
2013-06-25 23:56:30 -0300

    movement 1

--- src/js/game.js
+++ src/js/game.js
@@ -5,21 +5,33 @@ player = gamvas.Actor.extend({
         this.setFile(st.resource.getImage('rsc/'+ this.name + '.png'));
         var defaultState = this.getCurrentState();
 
-        var speed = 100;
+        var speed = 0;
+            accel = 0,
+            angle = 0;
+            friction = -1;
         
         defaultState.update = function(t){
-            if (gamvas.key.isPressed(contols))
-                console.log(p);
-            if (gamvas.key.isPressed(gamvas.key.LEFT))
-                this.actor.move(speed*t, 0);
+            accel = 0;
+            if (gamvas.key.isPressed(controls.up))
+                accel = 5;
+            if (gamvas.key.isPressed(controls.left)){
+                angle = (angle + 0.1) % (2 * Math.PI);
+                this.actor.setRotation(angle);
+            }
+            if (gamvas.key.isPressed(controls.right)){
+                angle = (angle - 0.1) % (2 * Math.PI);
+                this.actor.setRotation(angle);
+            }
+            speed = speed + accel
+            this.actor.move(Math.sin(angle) * speed * t, Math.cos(angle) * speed * t);
         }
     }
 });
 
 helloState = gamvas.State.extend({
         init: function(){
-            this.addActor(new player('p1', 10, 10, {'up': gamvas.key.UP, 'left': gamvas.key.LEFT}));
-            this.addActor(new player('p2', -20, 10, 3));
+            this.addActor(new player('p1', 10, 10, {'up': gamvas.key.UP, 'left': gamvas.key.LEFT, 'right': gamvas.key.RIGHT}));
+            //this.addActor(new player('p2', -20, 10, 3));
         },
         draw: function(t){
             this.c.fillStyle = '#fff';
